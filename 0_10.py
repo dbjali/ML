576d23696d202c7ddf3462e30c6ca329c33bdd5a
dbjali <dothan@daum.net>
2020-05-11 13:32:28 +0900

    three

---
+++ 0_10.py
@@ -0,0 +1,18 @@
+import numpy as np
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+        
+    # 훈련 데이터가 원-핫 벡터라면 정답 레이블의 인덱스로 반환
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+             
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+t = [0,0,1,0,0,0,0,0,0,0]
+y=[0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+cross_entropy_error(np.array(y), np.array(t))
+
+y=[0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+cross_entropy_error(np.array(y), np.array(t))
